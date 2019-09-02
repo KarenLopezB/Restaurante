3d9010adb57c5f96867d92434f098b5606478b0b
alumno <alumno@E303>
2019-09-02 12:40:49 -0700

    Agregar archivos de proyecto.

---
+++ Restaurante/Ingredientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    class Ingredientes
+    {
+        public string Nombre { get; set; }
+        public string Identificador { get; set; }
+
+        public Ingredientes() 
+        {
+            Nombre = "Pendiente";
+            Identificador = "Sin asignar";
+        }
+    }
+}
---
+++ Restaurante/Platillos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    class Platillos
+    {
+        public string NombrePla { get; set; }
+        public string IdentificadorPla { get; set; }
+        public string Descripcion { get; set; }
+
+        private int _precio;
+        public int Precio
+        {
+            get
+            {
+                return _precio;
+            }
+            set
+            {
+                if (value >= 50)
+                {
+                    _precio = value;
+                }
+                else
+                {
+                    throw new Exception("El precio debe ser mayor");
+                }
+            }
+        }
+
+        public List<Ingredientes> Ingredientes { get; set; }
+
+        public Platillos()
+        {
+            NombrePla = "Pendiente";
+            IdentificadorPla = "Sin asignar";
+            Descripcion = "Sin asignar";
+            Precio = 000;
+            Ingredientes = new List<Ingredientes>();
+        }
+    }
+}
